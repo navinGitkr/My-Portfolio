629cb3520e333e5e1d9a8471d1681e26adaa8555
unknown <navinm841424@gmail.com>
2023-11-24 14:29:57 +0530

    MyPortfolio commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+$(function() {
+    $('.chart').easyPieChart({
+      size: 140,
+      barColor: "aqua",
+      scaleLength: 0,
+      lineWidth: 15,
+      trackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+
+
+// nav show / hide
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('.navbar ul');
+
+
+
+
